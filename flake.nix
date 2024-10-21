59a0c73926403f83e0e6e16fdcc85bab8755df8b
Kymkim <cabraloliver2003@gmail.com>
2024-10-21 01:09:20 -0700

    Home Manager Initialization

--- flake.nix
+++ flake.nix
@@ -1,24 +1,40 @@
 {
-  description = "ukimNixOS Config";
+description = "ukimNixOS Config";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	home-manager.url = "github:nix-community/home-manager/master";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+};
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.workstation = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-        ./modules/core/hyprland.nix
-	./modules/core/rofi.nix
-	./modules/app/obsidian.nix	     
-	./modules/app/vscodium.nix
-	./modules/app/discord.nix
-	./modules/app/obs-studio.nix
-#	./modules/app/bitwarden.nix #Bitwarden seems to unable to compile in the unstable channel
-      ];
-    };
-  };
+outputs = { self, nixpkgs, home-manager,  ... }@inputs: {
+	nixosConfigurations = {
+		workstation = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			specialArgs = {inherit inputs;};
+			modules = [
+				./home-manager/home-manager.nix
+				./hosts/default/configuration.nix
+
+				./modules/core/hyprland.nix
+				./modules/core/rofi.nix
+				./modules/core/audio.nix
+	
+				./modules/app/obsidian.nix	     
+				./modules/app/vscodium.nix
+				./modules/app/discord.nix
+				./modules/app/obs-studio.nix
+#				./modules/app/bitwarden.nix #Bitwarden seems to unable to compile in the unstable channel
+			];
+		};
+	};
+	homeConfigurations = {
+		default =  home-manager.lib.homeManagerConfiguration { 
+			system = "x86_64-linux";
+			modules = [
+				./home-manager/default.nix
+			];
+		};
+	};
+};
 }
--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -1,6 +1,6 @@
+{ pkgs, ... }:
 {
-	hardware.pulseaudio = {
-		enable = true;
-		support32Bit = true;
-	};
+	environment.systemPackages = [
+		pkgs.pavucontrol
+	];
 }
