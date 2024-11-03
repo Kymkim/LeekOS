01583e4d74ee6b06fc0ca36ae1cd1517dfb3d8bf
Kymkim <cabraloliver18@outlook.com>
2024-11-02 21:22:35 -0700

    more reograniation

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -1,126 +1,32 @@
 { config, pkgs, inputs, ... }:
-
-# I USE DVORAK KEYBOARD LAYOUT. MOST PEOPLE DONT! PS CHANGE THE KEYBOARD LAYOUT!
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../modules/nixos/default.nix
-      inputs.home-manager.nixosModules.default
-    ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "MikuOS"; 
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  
-  # Bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-  #Polkit
-  security.polkit.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    #variant = "dvorak";
-  };
-
-  #console.keyMap = "dvorak";
-  console.keyMap = "us";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ukimnix = {
-    isNormalUser = true;
-    description = "Kim";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  #Enable Hyprland
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
-  home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users = {
-      "ukimnix" = import ./home.nix;
-    };
-    backupFileExtension = "backup";
-  };
-
-
-  # ░░      ░░░       ░░░       ░░░░      ░░
-  # ▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒
-  # ▓  ▓▓▓▓  ▓▓       ▓▓▓       ▓▓▓▓      ▓▓
-  # █        ██  ████████  ██████████████  █
-  # █  ████  ██  ████████  █████████      ██
-
-  #########################################################                                      
-  # Applications. I dont really see a point               #
-  # on having these on the home.nix. I only have one      #
-  # user on my PC which is me...                          #
-  # Of course, feel free to remove if you dont need these #
-  #########################################################
+  imports = [ 
+    ./hardware-configuration.nix  #DO NOT USE MINE! USE YOUR OWN HARDWARE CONFIGURATION
+    ../../modules/nixos/default.nix
+    inputs.home-manager.nixosModules.default
+  ];
 
   steam.enable = true;
   printer.enable = true;
+  gtk-theme.enable = true;
   vms.enable = true;
   thunar.enable = true;
-
+  
   environment.systemPackages = with pkgs; [
-    
-    #Will move all of these as a separate .nix for modularity later
-    #You need these for sure   
+    #Most of these will be moved once they got their own configuration settings
+    #In the meantime, these are appllications I like to run on my system
+    #Please comment out the ones you dont need...
     rofi-wayland  #Temporarily here. Will use EWW or AGS for app launcher down the line
     nerdfonts     #Icons as fonts
     nwg-look      #For setting GTK themes
     pavucontrol   #GUI for PulseAudio 
     blueberry     #GUI for configuring Bluetooth Devices
-
-    #Generally good to have in your system
     git
     vim
     wget
     curl
     firefox
     killall     
-
-    #Optional. Comment these out if not needed
     gh                        #GitHub CLI Tool
     obsidian                  #Note taking app
     vscodium                  #Coding app
@@ -132,14 +38,72 @@
     kdePackages.kdenlive
     bottles
     minecraft
-    
-
   ];
 
 
+  #System Configuration:
+  
+    # Environment Variables
+    environment.variables.EDITOR = "vim";
 
-  environment.variables.EDITOR = "vim";
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  system.stateVersion = "24.05";
+    networking = {
+      hostName = "MikuOS"; 
+      networkmanager.enable = true;
+    };
+    
+    # Bluetooth
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+
+    # Polkit
+    security.polkit.enable = true;
+
+    # Locale and Keyboard
+    time.timeZone = "America/Los_Angeles";
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+    console.keyMap = "us";
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.ukimnix = {
+      isNormalUser = true;
+      description = "Kim";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [];
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # Enable flakes
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    # Home Manager Configuration
+    home-manager = {
+      extraSpecialArgs = {inherit inputs;};
+      users = {
+        "ukimnix" = import ./home.nix;
+      };
+      backupFileExtension = "backup";
+    };
+
+    # No touchie
+    system.stateVersion = "24.05";
 
 }
