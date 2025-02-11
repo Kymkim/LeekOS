6ad6b3f1ad7388a5e7b45950ff90877e6272225c
Kymkim <cabraloliver18@outlook.com>
2025-02-10 22:59:55 -0800

    init files

--- flake.nix
+++ flake.nix
@@ -1,53 +1,51 @@
 {
-description = "ukimNixOS Config";
+  description = ''
+    For questions just DM me on X: https://twitter.com/@m3tam3re
+    There is also some NIXOS content on my YT channel: https://www.youtube.com/@m3tam3re
 
-inputs = {
-	nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	home-manager.url = "github:nix-community/home-manager/master";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
-	ags.url = "github:Aylur/ags";
-};
+    One of the best ways to learn NIXOS is to read other peoples configurations. I have personally learned a lot from Gabriel Fontes configs:
+    https://github.com/Misterio77/nix-starter-configs
+    https://github.com/Misterio77/nix-config
 
-outputs = { self, nixpkgs, home-manager,  ... }@inputs: 
-	let
-		lib = nixpkgs.lib;
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
-	in {
+    Please also check out the starter configs mentioned above.
+  '';
 
-		#NixOsConfigurations contains all of the applications that I need
-		#In my case I have two config, one for my FrameworkLaptop16 and one for my PC
-		nixosConfigurations = {
-			default = lib.nixosSystem {
-				inherit system;
-				specialArgs = {inherit inputs;};
-				modules = [
-					./hosts/default/system.nix
-					inputs.home-manager.nixosModules.default
-				];
-			};
-			fw16 = lib.nixosSystem {
-				inherit system;
-				specialArgs = {inherit inputs;};
-				modules = [
-					./hosts/framework16/system.nix	
-					inputs.home-manager.nixosModules.default
-				];
-			};
-		};
-	};
+  inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
 
-	#Is it Miku Flow Chart
-	#
-	#twin tails?
-	# |     |
-	# yes   no
-	# |     |
-	# |     not miku.
-	# |
-	# is it blue?
-	# |     |
-	# yes   no
-	# |     |
-	# its miku regardless.
+  outputs = { self, home-manager, nixpkgs, ... }@inputs:
+    let
+      inherit (self) outputs;
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      overlays = import ./overlays { inherit inputs; };
+      nixosConfigurations = {
+        your-host = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/your-host ];
+        };
+      };
+      homeConfigurations = {
+        "your-name@your-host" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home/your-name/your-host.nix ];
+        };
+      };
+    };
 }
