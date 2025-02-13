261c06942346966054f04d6544374cfc541996ad
Kymkim <oliver@kym-kim.com>
2025-02-13 13:54:57 -0800

    added more essential stuff

--- hosts/fw16/configuration.nix
+++ hosts/fw16/configuration.nix
@@ -75,7 +75,23 @@
     webcord
     libreoffice
     xfce.thunar
+    pavucontrol
+    plexamp
+    zoom-us
   ];
+
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  programs.thunar.enable = true;
+  programs.xfconf.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+    thunar-volman
+  ];
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+
 
   services.tailscale.enable = true;
 
